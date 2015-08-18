af8a1ab8c146ddaabf8bd2174d481a03c0e42754
Stefan Theard <smtheard@gmail.com>
2015-08-18 15:51:48 -0500

    added fast version of addDigits

--- Add Digits/solution.java
+++ Add Digits/solution.java
@@ -6,7 +6,12 @@ class Solution {
         return num;
     }
 	
+	public static int addDigitsFast(int num) {
+		return (num - 1) % 9 + 1;
+	}
+
 	public static void main(String[] args) {
-		System.out.println(addDigits(38));
+		System.out.println(addDigits(63829));
+		System.out.println(addDigitsFast(63829));
 	}
 }
