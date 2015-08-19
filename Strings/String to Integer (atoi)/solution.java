8fcdca275e26fa38e76d97841393d4780e7c022e
Stefan Theard <smtheard@gmail.com>
2015-08-19 18:31:46 -0500

    working implementation but not as robust as needed

--- Strings/String to Integer (atoi)/solution.java
+++ Strings/String to Integer (atoi)/solution.java
@@ -1,9 +1,40 @@
+import java.lang.*;
+
 class Solution {
-    public int myAtoi(String str) {
-        
+	public static int power(int base, int pow) {
+		if(pow == 0)
+			return 1;
+		return base * power(base, --pow);
+	}
+
+	public static int commaCount(String str){
+		int count = 0;
+		for(int i = 0; i < str.length(); i++) {
+			if(str.charAt(i) == ',' )
+				count++;
+		}
+		return count;
+	}
+
+    public static int myAtoi(String str) {
+    	int result = 0;
+    	int pow = str.length() - 1 - commaCount(str);
+    	for(int i = 0; i < str.length(); i++){
+    		if(str.charAt(i) == ',' ){
+    			pow++;
+    			continue;
+    		}
+    		else if(str.charAt(i) == '.'){
+    			return result/power(10, pow);
+    		}
+    		result = result + (str.charAt(i) - 48) * power(10, (pow - i));
+    	}
+        return result;
     }
 	
 	public static void main(String[] args) {
-		
+		System.out.println(myAtoi("5175"));
+		System.out.println(myAtoi("57,182"));
+		System.out.println(myAtoi("5.00"));
 	}
 }
