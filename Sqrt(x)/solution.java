68ab25b8e891a5953724e4f144bc585424ddd1c1
Stefan Theard <smtheard@gmail.com>
2015-08-22 01:59:15 -0500

    failing a few cases, implementation works for perfect squares however

--- Sqrt(x)/solution.java
+++ Sqrt(x)/solution.java
@@ -1,10 +1,26 @@
 class Solution {
+
     public static int mySqrt(int x) {
-        return 4;
+    	for(int i = 0; i <= x; i++){
+    		if(x - (i * i) <= 0){
+    			return i;
+    		}
+    	}
+    	return -1;
     }
 
 	public static void main(String[] args) {
-		System.out.println(mySqrt(16)); //4
+		System.out.println(mySqrt(0)); //0
+		System.out.println(mySqrt(1)); //1
 		System.out.println(mySqrt(2)); //1
+		System.out.println(mySqrt(3)); //1
+		System.out.println(mySqrt(4)); //2
+		System.out.println(mySqrt(5)); //2
+		System.out.println(mySqrt(6)); //2
+		System.out.println(mySqrt(8)); //2
+		System.out.println(mySqrt(9)); //3
+		System.out.println(mySqrt(16)); //4
+		System.out.println(mySqrt(81)); //9
+		System.out.println(mySqrt(2147483647)); //46340
 	}
 }
