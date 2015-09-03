ced76a13239e2abfc833105186c660ebe9d23ec6
Stefan Theard <smtheard@gmail.com>
2015-09-02 20:30:59 -0500

    adding new leetcode problem

---
+++ Strings/Length of Last Word/solution.java
@@ -0,0 +1,9 @@
+class Solution {
+    public static int lengthOfLastWord(String s) {
+        return 5;
+    }
+	
+	public static void main(String[] args) {
+		System.out.println(lengthOfLastWord("Hello World")); //5
+	}
+}
