c906886d0ccb4d1feacd8e45524ad5a47827951b
Stefan Theard <smtheard@gmail.com>
2015-09-03 14:11:30 -0500

    solution checked in

--- Strings/Length of Last Word/solution.java
+++ Strings/Length of Last Word/solution.java
@@ -1,9 +1,17 @@
 class Solution {
     public static int lengthOfLastWord(String s) {
-        return 5;
+    	int length = 0;
+    	for(int i = 0; i < s.length(); i++){
+    		length++;
+    		if(s.charAt(i) == ' ')
+    			length = 0;
+    	}
+        return length;
     }
 	
 	public static void main(String[] args) {
 		System.out.println(lengthOfLastWord("Hello World")); //5
+		System.out.println(lengthOfLastWord("Hello World be")); //2
+		System.out.println(lengthOfLastWord("Hello to the World of Word")); //4
 	}
 }
