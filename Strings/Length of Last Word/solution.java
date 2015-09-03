dee64f6fa2f5d997b9f7d8ac3f3c4c82da0b3970
Stefan Theard <smtheard@gmail.com>
2015-09-03 14:17:58 -0500

    added failing case, misinterpreted specifications

--- Strings/Length of Last Word/solution.java
+++ Strings/Length of Last Word/solution.java
@@ -2,17 +2,18 @@ class Solution {
     public static int lengthOfLastWord(String s) {
     	int length = 0;
     	for(int i = 0; i < s.length(); i++){
-    		length++;
-    		if(s.charAt(i) == ' ')
-    			length = 0;
+            length++;
+            if(s.charAt(i) == ' ')
+                length = 0;
     	}
         return length;
     }
 	
 	public static void main(String[] args) {
+        System.out.println(lengthOfLastWord("a ")); //1
+        System.out.println(lengthOfLastWord("Hello World be")); //2
+        System.out.println(lengthOfLastWord("Hello to the World of Word")); //4
 		System.out.println(lengthOfLastWord("Hello")); //5
 		System.out.println(lengthOfLastWord("Hello World")); //5
-		System.out.println(lengthOfLastWord("Hello World be")); //2
-		System.out.println(lengthOfLastWord("Hello to the World of Word")); //4
 	}
 }
