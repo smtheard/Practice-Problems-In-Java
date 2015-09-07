84985665c890ff64298fe878ef2efc87202f8378
Stefan Theard <smtheard@gmail.com>
2015-09-06 23:17:04 -0500

    prototyping potential solution

--- Ugly Number/solution.java
+++ Ugly Number/solution.java
@@ -1,7 +1,18 @@
-
+import java.util.*;
 class Solution {
+	public static ArrayList<Integer> primeFactors(int num){
+		ArrayList<Integer> primes = new ArrayList<Integer>();
+		primes.add(2);
+		primes.add(3);
+		primes.add(5);
+		return primes;
+	}
 	public static boolean isUgly(int num) {
-        return true;
+		ArrayList<Integer> primes = primeFactors(num);
+        return primes.contains(2) && 
+        	   primes.contains(3) &&
+        	   primes.contains(5) &&
+        	   primes.size() == 3;
     }
 
 	public static void main(String[] args) {
