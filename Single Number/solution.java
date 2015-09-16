c9246d1b4217f4d4d9cd32caf9f31a2f5b11972b
Stefan Theard <smtheard@gmail.com>
2015-09-16 16:14:44 -0500

    new problem.. Single Number

---
+++ Single Number/solution.java
@@ -0,0 +1,9 @@
+class Solution {
+	public static int singleNumber(int[] nums) {
+        return 3;
+    }
+	public static void main(String[] args) {
+		int arr[] = {1, 1, 2, 2, 4, 3, 5, 6, 4, 5, 6};
+		System.out.println(singleNumber(arr)); //3
+	}
+}
