3d8fe333611e599132dd37986e14fc42885d36c9
Stefan Theard <smtheard@gmail.com>
2015-09-17 16:28:52 -0500

    solution works but is far too slow.

--- Single Number/solution.java
+++ Single Number/solution.java
@@ -3,7 +3,7 @@ import java.util.*;
 class Solution {
 	public static int singleNumber(int[] nums) {
 		ArrayList<Integer> single = new ArrayList<Integer>();
-		for(int i = 0; i < nums.length - 1; i++)
+		for(int i = 0; i < nums.length; i++)
 			if(single.contains(nums[i]))
 				single.remove(single.indexOf(nums[i]));
 			else
@@ -11,9 +11,10 @@ class Solution {
         return single.get(0);
     }
 
-
 	public static void main(String[] args) {
 		int arr[] = {1, 1, 2, 2, 4, 3, 5, 6, 4, 5, 6};
 		System.out.println(singleNumber(arr)); //3
+		int arr2[] = {1};
+		System.out.println(singleNumber(arr2)); //1
 	}
 }
