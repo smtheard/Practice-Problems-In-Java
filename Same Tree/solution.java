c376f4611d3493484cda756c91f05a6487ec1176
Stefan Theard <smtheard@gmail.com>
2015-09-18 09:22:09 -0500

    solved, solution beats 85 percent of other java solutions by speed

--- Same Tree/solution.java
+++ Same Tree/solution.java
@@ -1,6 +1,11 @@
 class Solution {
 	public boolean isSameTree(TreeNode p, TreeNode q) {
-		return false;
+		if(p == null && q == null)
+			return true;
+		else if((p == null && q != null) || (p != null && q == null))
+			return false;
+		else
+        	return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 
 	public static void main(String[] args) {
