80d5f9264145d6656b8610e531c6e42faccbf127
Stefan Theard <smtheard@gmail.com>
2015-09-19 23:17:59 -0500

    new problem

---
+++ Move Zeroes/solution.java
@@ -0,0 +1,11 @@
+class Solution {
+	public static void moveZeroes(int[] nums) {
+        return;
+    }
+
+	public static void main(String[] args) {
+		int[] arr = {0, 1, 0, 3, 12};
+        moveZeroes(arr);
+        System.out.println(arr); //[1, 3, 12, 0, 0]
+	}
+}
