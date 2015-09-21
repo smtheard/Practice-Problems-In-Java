2dcacce16a1b7e545e957b81618f70fa4d817563
Stefan Theard <smtheard@gmail.com>
2015-09-21 09:03:52 -0500

    modified formatting

--- Move Zeroes/solution.java
+++ Move Zeroes/solution.java
@@ -3,10 +3,9 @@ class Solution {
         int numZeroes = 0;
         for(int i = nums.length - 1; i > -1 ; i--){
             if(nums[i] == 0){
-                for(int j = i; j < nums.length - 1; j++){
+                for(int j = i; j < nums.length - 1; j++)
                     nums[j] = nums[j + 1];
-                }
-                nums[nums.length - numZeroes - 1] = 0;
+                nums[nums.length - 1 - numZeroes] = 0;
                 numZeroes++;
             }
         }
