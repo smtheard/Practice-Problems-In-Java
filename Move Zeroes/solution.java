87f99490a7ef5d3e5ff8f60b3ecceeed4e9f930e
Stefan Theard <smtheard@gmail.com>
2015-09-20 22:51:32 -0500

    made some headway but actually misread one part of the specs. will continue tomorrow

--- Move Zeroes/solution.java
+++ Move Zeroes/solution.java
@@ -1,11 +1,22 @@
 class Solution {
 	public static void moveZeroes(int[] nums) {
+        int max = nums.length;
+        int numZeroes = 0;
+        for(int i = 0; i < max; i++){
+            if(nums[i] == 0){
+                nums[i] = nums[nums.length - numZeroes - 1];
+                nums[nums.length - numZeroes - 1] = 0;
+                max--;
+                numZeroes++;
+            }
+        }
         return;
     }
 
 	public static void main(String[] args) {
 		int[] arr = {0, 1, 0, 3, 12};
         moveZeroes(arr);
-        System.out.println(arr); //[1, 3, 12, 0, 0]
+        for(int i = 0; i < arr.length; i++)
+            System.out.print(arr[i] + " "); //[1, 3, 12, 0, 0]
 	}
 }
