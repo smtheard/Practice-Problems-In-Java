763392c997fb738eb7a80e09962f838ec212163d
Stefan Theard <smtheard@gmail.com>
2015-09-22 14:58:52 -0500

    added new problem

---
+++ Best Time to Buy and Sell Stock II/solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public static int maxProfit(int[] prices) {
+        return 1;
+    }
+
+	public static void main(String[] args) {
+		int[] arr = {0, 1, 0, 3, 12};
+        System.out.println(maxProfit(arr));
+	}
+}
