ced58eb59ece940fe6a8b6b95e7d63efaed480f9
Stefan Theard <smtheard@gmail.com>
2015-09-23 21:36:38 -0500

    incomplete, this would be the max profit in a single trade, need to optimize for multiple trades

--- Best Time to Buy and Sell Stock II/solution.java
+++ Best Time to Buy and Sell Stock II/solution.java
@@ -1,10 +1,18 @@
 class Solution {
     public static int maxProfit(int[] prices) {
-        return 1;
+    	int min = 0;
+    	int max = 0;
+        for(int i = 0; i < prices.length; i++){
+        	if(prices[i] > max)
+        		max = prices[i];
+        	if(prices[i] < min)
+        		min = prices[i];
+        }
+        return max - min;
     }
 
 	public static void main(String[] args) {
 		int[] arr = {0, 1, 0, 3, 12};
-        System.out.println(maxProfit(arr));
+        System.out.println(maxProfit(arr)); //13
 	}
 }
