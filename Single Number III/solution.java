ccb9729a5b3f45bc349d59c1718ca28d83749468
Stefan Theard <smtheard@gmail.com>
2015-09-24 16:20:04 -0500

    added single number III

---
+++ Single Number III/solution.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+	public static int[] singleNumber(int[] nums) {
+        int arr[] = {3, 5};
+        return arr;
+    }
+
+	public static void main(String[] args) {
+		int arr[] = {1, 2, 1, 3, 2, 5};
+		int[] result = singleNumber(arr);
+		System.out.println(result[0] + " " + result[1]); //{3, 5}
+	}
+}
