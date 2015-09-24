064c302d8b931ba870ac4c4abfae1c50f646a466
Stefan Theard <smtheard@gmail.com>
2015-09-24 16:23:44 -0500

    added contains duplicate

---
+++ Contains Duplicate/solution.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+	public static boolean containsDuplicate(int[] nums) {
+        return true;
+    }
+
+	public static void main(String[] args) {
+		int arr[] = {1, 2, 1, 3, 2, 5};
+		System.out.println(containsDuplicate(arr)); //true
+	}
+}
