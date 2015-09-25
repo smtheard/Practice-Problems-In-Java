5a4fd559e0ac8a18211f3694f4aedcdcb210510b
Stefan Theard <smtheard@gmail.com>
2015-09-24 20:18:29 -0500

    implemented rough outline of solution, need to implement merge sort

--- Contains Duplicate/solution.java
+++ Contains Duplicate/solution.java
@@ -1,8 +1,18 @@
 import java.util.*;
 
 class Solution {
+
+	public static int[] mergeSort(int[] nums){
+		int[] arr = {1, 1, 2, 2, 3, 5};
+		return arr;
+	}
+
 	public static boolean containsDuplicate(int[] nums) {
-        return true;
+		int[] sortedNums = mergeSort(nums);
+		for(int i = 0; i < nums.length - 1; i++)
+			if(sortedNums[i] == sortedNums[i + 1])
+				return true;
+        return false;
     }
 
 	public static void main(String[] args) {
