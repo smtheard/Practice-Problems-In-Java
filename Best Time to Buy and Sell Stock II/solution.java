10ca463020342e28d679bce2d740d87a1e3de160
Stefan Theard <smtheard@gmail.com>
2015-09-24 20:25:41 -0500

    added solution, needs more test cases

--- Best Time to Buy and Sell Stock II/solution.java
+++ Best Time to Buy and Sell Stock II/solution.java
@@ -1,18 +1,16 @@
 class Solution {
     public static int maxProfit(int[] prices) {
-    	int min = 0;
-    	int max = 0;
-        for(int i = 0; i < prices.length; i++){
-        	if(prices[i] > max)
-        		max = prices[i];
-        	if(prices[i] < min)
-        		min = prices[i];
+    	int profit = 0;
+        for(int i = 0; i < prices.length - 1; i++){
+        	if(prices[i] < prices[i + 1]){
+                profit = profit + (prices[i + 1] - prices[i]);
+            }
         }
-        return max - min;
+        return profit;
     }
 
 	public static void main(String[] args) {
 		int[] arr = {0, 1, 0, 3, 12};
-        System.out.println(maxProfit(arr)); //13 in three trades, 12 in one trade
+        System.out.println(maxProfit(arr)); //13 in three trades
 	}
 }
