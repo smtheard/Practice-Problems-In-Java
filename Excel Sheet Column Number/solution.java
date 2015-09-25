90b56b329d6d9a801495e50a9866261f2f7a289f
Stefan Theard <smtheard@gmail.com>
2015-09-24 20:48:40 -0500

    i believe this is a working o(1) solution

--- Excel Sheet Column Number/solution.java
+++ Excel Sheet Column Number/solution.java
@@ -1,12 +1,22 @@
 import java.util.*;
 
 class Solution {
-	public int titleToNumber(String s) {
-        return 1;
+	public static int titleToNumber(String s) {
+        return s.length() * 26 - 26 + (s.charAt(s.length() - 1) - 64);
     }
 
 	public static void main(String[] args) {
 		String input = "A";
 		System.out.println(titleToNumber(input)); //1
+		input = "B";
+		System.out.println(titleToNumber(input)); //2
+		input = "Z";
+		System.out.println(titleToNumber(input)); //26
+		input = "AA";
+		System.out.println(titleToNumber(input)); //27
+		input = "AB";
+		System.out.println(titleToNumber(input)); //28
+		input = "AAA";
+		System.out.println(titleToNumber(input)); //53
 	}
 }
