f2408870ebd2a767551f50a25abdebb4c606b361
Stefan Theard <smtheard@gmail.com>
2015-09-25 18:57:34 -0500

    added perfect squares problem

---
+++ Perfect Squares/solution.java
@@ -0,0 +1,10 @@
+class Solution {
+	public static int numSquares(int n) {
+        return 2;
+    }
+
+	public static void main(String[] args) {
+		System.out.println(numSquares(12)); //3
+		System.out.println(numSquares(13)); //2
+	}
+}
