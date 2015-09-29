2cfcdcaca9035b8586e6b21cf1d88f68b635c84b
Stefan Theard <smtheard@gmail.com>
2015-09-29 15:24:55 -0500

    added comment with given treenode definition

--- Invert Binary Tree/solution.java
+++ Invert Binary Tree/solution.java
@@ -1,3 +1,13 @@
+/** Given:
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
 class Solution {
 	public TreeNode invertTree(TreeNode root) {
         if(root == null)
