b419c6a11dd1666f63941c188ff72a0c3bbbddd3
Stefan Theard <smtheard@gmail.com>
2015-09-29 15:13:09 -0500

    added merge sort and working solution

--- Contains Duplicate/solution.java
+++ Contains Duplicate/solution.java
@@ -1,16 +1,35 @@
 import java.util.*;
 
 class Solution {
-
-	public static int[] mergeSort(int[] nums){
-		int[] arr = {1, 1, 2, 2, 3, 5};
-		return arr;
-	}
+	public static void mergeSort(int array[], int start, int end) {
+        int middle, left, right, temp;
+        if (start < end) {
+            middle = (start + end) / 2;
+            mergeSort(array, start, middle);
+            mergeSort(array, middle + 1, end);
+            left = start;
+            right = middle + 1;
+            while (left <= middle && right <= end) {
+                if (array[left] > array[right]) {
+                    temp = array[right];
+                    for (int i = right - 1; i >= left; i--) {
+                        array[i + 1] = array[i];
+                    }
+                    array[left] = temp;
+                    right++;
+                    middle++;
+                }
+                left++;
+            }
+        }
+    }
 
 	public static boolean containsDuplicate(int[] nums) {
-		int[] sortedNums = mergeSort(nums);
+		if(nums.length < 2)
+			return false;
+		mergeSort(nums, 0, nums.length - 1);
 		for(int i = 0; i < nums.length - 1; i++)
-			if(sortedNums[i] == sortedNums[i + 1])
+			if(nums[i] == nums[i + 1])
 				return true;
         return false;
     }
@@ -18,5 +37,9 @@ class Solution {
 	public static void main(String[] args) {
 		int arr[] = {1, 2, 1, 3, 2, 5};
 		System.out.println(containsDuplicate(arr)); //true
+		int arr2[] = {1, 2, 1, 3, 5, 5};
+		System.out.println(containsDuplicate(arr2)); //true
+		int arr3[] = {0, 1, 2};
+		System.out.println(containsDuplicate(arr3)); //false
 	}
 }
