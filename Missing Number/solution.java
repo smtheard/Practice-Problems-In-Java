c5d5b3f8e1b44fcb39bcca355378d90761a058e1
Stefan Theard <smtheard@gmail.com>
2015-10-01 20:33:05 -0500

    added missing number problem

---
+++ Missing Number/solution.java
@@ -0,0 +1,9 @@
+class Solution {
+	public int missingNumber(int[] nums) {
+        return 2;
+    }
+	public static void main(String[] args) {
+		int[] arr = {0, 1, 3};
+		System.out.println(missingNumber(arr)); //2
+	}
+}
