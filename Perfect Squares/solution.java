bea6ae4188fbdd51dbe31f811a17b26c70fbcdcd
Stefan Theard <smtheard@gmail.com>
2015-10-02 22:18:30 -0500

    added needed auxiliary method, still working on solution

--- Perfect Squares/solution.java
+++ Perfect Squares/solution.java
@@ -1,6 +1,9 @@
 class Solution {
+	public static boolean isPerfectSquare(int n){
+		return (Math.sqrt(n) * Math.sqrt(n) == n);
+	}
 	public static int numSquares(int n) {
-        return 2;
+		return 2;
     }
 
 	public static void main(String[] args) {
