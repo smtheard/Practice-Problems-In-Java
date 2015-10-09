b1edb1db38f9402aabce8d2d26289b80b58f1da3
Stefan Theard <smtheard@gmail.com>
2015-10-08 23:08:58 -0500

    fixed spacing

--- Two Sum/solution.java
+++ Two Sum/solution.java
@@ -4,8 +4,8 @@ class Solution {
         for(int i = 0; i < nums.length; i++){
         	for(int j = 0; j < nums.length; j++){
         		if(nums[i] + nums[j] == target && i != j){
-        			arr[0] = i+1;
-        			arr[1] = j+1;
+        			arr[0] = i + 1;
+        			arr[1] = j + 1;
         			return arr;
         		}
         	}
